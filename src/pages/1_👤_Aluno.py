27bc43707426bdff962b4eea32e0a29eeaa77fcc
Marcio Bigolin <marcio.bigolin@gmail.com>
2024-03-08 02:25:12 -0300

    Refact #4

--- src/pages/1_👤_Aluno.py
+++ src/pages/1_👤_Aluno.py
@@ -2,47 +2,116 @@ import enyalius as eny
 import streamlit as st
 import pandas as pd
 
+_ = eny.loadLang("Home", "pt-br")
+
 
 st.set_page_config(page_title="Análise por aluno", page_icon="📈", layout="wide")
 
 params = st.query_params
 schemaUsuario = params.get('usuario', 'moodle_marcio2')  
-aluno = params.get('aluno', '276')
+aluno = params.get('aluno', 101)
 
-
-def dataFrame(aluno):
-    from sqlalchemy import text
-    conn = eny.conecta()
-
-    sqlw = f"SELECT  texto_extraido, coh_frazier, coh_brunet, data_entrega, nota FROM {schemaUsuario}.tarefa_fato tf INNER JOIN {schemaUsuario}.aluno a ON a.id = tf.aluno_id WHERE aluno_id = {aluno};"
-
-    # Perform query.
-    sql_query =  pd.read_sql_query(sql=text(sqlw), con=conn)
-    df = pd.DataFrame(sql_query, columns = ['titulo', 'texto_extraido', 'coh_frazier', 'coh_brunet', 'data_entrega', 'nota'])
-
-    return df
-
-def dataTurma(aluno):
-    conn = eny.conecta()
 
 def dataMundo():
     from sqlalchemy import text
     conn = eny.conecta()
 
-    sqlw = f"SELECT texto_extraido, coh_frazier, coh_brunet, data_entrega, nota, aluno_id FROM {schemaUsuario}.tarefa_fato WHERE LENGTH(texto_extraido) > 30;"
+    sqlw = f"SELECT texto_extraido, coh_frazier, coh_brunet, data_entrega, nota, turma_id, aluno_id, nome_completo FROM {schemaUsuario}.tarefa_fato tf INNER JOIN {schemaUsuario}.aluno a ON a.id = aluno_id WHERE LENGTH(texto_extraido) > 30;"
 
     # Perform query.
     sql_query =  pd.read_sql_query(sql=text(sqlw), con=conn)
-    df = pd.DataFrame(sql_query, columns = ['titulo', 'texto_extraido', 'coh_frazier', 'coh_brunet', 'data_entrega', 'nota'])
+    df = pd.DataFrame(sql_query, columns = ['titulo', 'texto_extraido', 'coh_frazier', 'coh_brunet', 'data_entrega', 'nota', 'nome_completo', 'aluno_id'])
 
     return df
 
-st.title(f"Avaliação de {aluno}")
+def analiseMetrica(data):
+
+    # Mapeamento de valores internos para rótulos
+    mapa_rotulos = {
+        'coh_frazier': 'Complexidade Sintática de Frazier',
+        'coh_brunet': 'Indice de Brunet',
+    }
+
+    # Inverta o dicionário para mapear rótulos para valores internos
+    mapa_valores = {v: k for k, v in mapa_rotulos.items()}
+
+    # Use os rótulos no selectbox
+    rotulo_selecionado = st.selectbox(
+        'Por favor, selecione a métrica que deseja visualizar:',
+        list(mapa_rotulos.values())
+    )
+
+    # Mapeie o rótulo de volta para o valor interno
+    metrica = mapa_valores[rotulo_selecionado]
+
+    col1, col2 = st.columns([4, 8])
+    with col1:
+        import requests
+
+        metrica_sem_prefixo = metrica.replace('coh_', '')
+
+        # Faça uma solicitação GET para buscar a descrição da métrica
+        response = requests.get(f'https://revisaoonline.com.br/public/verMetrica/{metrica_sem_prefixo}')
+
+        if response.status_code == 200:
+            st.markdown(response.content.decode() , unsafe_allow_html= True)
+    with col2:
+  
+        media = df[metrica].mean()
+
+        import plotly.graph_objects as go
+
+        fig = go.Figure()
+        fig.add_trace(go.Histogram(x=data[metrica]))
+
+        fig.add_shape(
+            type="line",
+            x0=media,
+            x1=media,
+            y0=0,
+            y1=1,
+            yref="paper",
+            line=dict(
+                color="Red",
+                width=3,
+                dash="dashdot",
+            )
+        )
+
+        st.plotly_chart(fig)
+        st.write('A análise de histograma e gráfico mostra que a média da métrica é: ', media)
+
+
+
+
+dfMundo= dataMundo()
+
+
+
+# Cria um dicionário onde as chaves são os IDs dos alunos e os valores são os nomes dos alunos
+alunos_dict = pd.Series(dfMundo.nome_completo.values,index=dfMundo.aluno_id).to_dict()
+
+
+
+st.sidebar.title(_("Filtre os dados"))
+keys_list = list(alunos_dict.keys())
+aluno_index = keys_list.index(int(aluno))
+
+aluno = st.sidebar.selectbox(
+        _('Selecione um aluno para continuar?'),
+        options=keys_list,
+        index=aluno_index,
+        format_func=lambda x: alunos_dict[x]
+)
+df = dfMundo.loc[dfMundo['aluno_id'] == aluno]
+
+
+
+st.title(f"Avaliação de {alunos_dict[aluno]}")
 
 indiTab, turmaTab, mundoTab = st.tabs(["Individual", "Turma", "Mundo"])
 
 with indiTab:
-    df = dataFrame(aluno)
     col1, col2 = st.columns([8, 4])
     with col1:
         st.header("Últimos registros")
@@ -55,28 +124,10 @@ with indiTab:
 
     st.bar_chart(chart_data)
 
+with turmaTab:
+    st.header("Análise de métricas por turma")
+    # data = dataTurma(aluno)
+    # analiseMetrica(data)
+
 with mundoTab:
-    df = dataMundo()
-
-    media = dataFrame(aluno)['coh_frazier'].mean()
-
-    import plotly.graph_objects as go
-    
-    fig = go.Figure()
-    fig.add_trace(go.Histogram(x=df['coh_frazier']))
-
-    fig.add_shape(
-        type="line",
-        x0=media,
-        x1=media,
-        y0=0,
-        y1=1,
-        yref="paper",
-        line=dict(
-            color="Red",
-            width=3,
-            dash="dashdot",
-        )
-    )
-
-    st.plotly_chart(fig)
+    analiseMetrica(dfMundo)
